0ccc95d75230521e152cd282b13a28cdd0a0aa9e
Mathew Thomas <mathew1.9thomas@gmail.com>
2022-11-21 18:04:48 +0530

    refactoring keyword_parser.rb

--- keyword_parser.rb
+++ keyword_parser.rb
@@ -1,51 +1,21 @@
 require_relative "./lib/language_parser"
 
 class KeywordParser
-  attr_reader :args, :keywords
+  attr_reader :args, :keywords, :input_bytes
 
   def initialize
     @args = ARGV
     @keywords = {}
+    begin
+      validate_args
+      read_input_file
+      read_keywords
+    rescue
+      # Do nothing and stop execution
+    end
   end
 
-  def call
-    if args.size != 3
-      print "Arguments - parser source_file language keywords_file\n"
-      return
-    end
-
-    begin
-      bytes = File.read(args[0])
-    rescue => err
-      print "Error reading input file, #{err}\n"
-      return
-    end
-
-    begin
-      read_keywords
-    rescue => err
-      print "Error reading keywords #{err}\n"
-      return
-    end
-
-    p processedString = LanguageParser.run(
-        byte_string: bytes,
-        keywords: keywords,
-        language: args[1]
-      )
-    temp_file_path = "#{args[0]}.tmp"
-
-    File.delete(temp_file_path) if File.exist?(temp_file_path)
-    File.open(temp_file_path, 'w') { |file| file.write(processedString) }
-
-
-    case args[1]
-    when "ruby"
-      command = "ruby"
-    else
-      command = "run"
-    end
-
+  def execute
     # running script
     begin
       output = `#{command} #{temp_file_path}`
@@ -58,16 +28,68 @@ class KeywordParser
     print "Script Output - \n#{output}"
   end
 
+  def parse
+    print "Parsed code -\n#{parsed_string}"
+  end
+
   private
-    def read_keywords
-      file = File.open(args[2])
-      file.readlines.map(&:chomp).each do |line|
-        fields = line.split(" ")
-        next if fields.size != 2
-        @keywords[fields[0]] = fields[1]
+
+    def validate_args
+      if args.size != 3
+        print "Arguments - parser source_file language keywords_file\n"
+        raise
       end
+    end
+
+    def read_input_file
+      begin
+        @input_bytes = File.read(args[0])
+      rescue => err
+        print "Error reading input file, #{err}\n"
+        raise
+      end
+    end
+
+    def read_keywords
+      begin
+        file = File.open(args[2])
+        file.readlines.map(&:chomp).each do |line|
+          fields = line.split(" ")
+          next if fields.size != 2
+          @keywords[fields[0]] = fields[1]
+        end
+      rescue => err
+        print "Error reading keywords #{err}\n"
+        raise
+      end
+    end
+
+    def parsed_string
+      @parsed_string ||= LanguageParser.run(
+                            byte_string: input_bytes,
+                            keywords: keywords,
+                            language: args[1]
+                          )
+    end
+
+    def command
+      @command ||=   case args[1]
+                      when "ruby"
+                        "ruby"
+                      else
+                        "run"
+                      end
+    end
+
+    def temp_file_path
+      @temp_file_path ||= (
+        file_path = "#{args[0]}.tmp"
+        File.delete(file_path) if File.exist?(file_path)
+        File.open(file_path, 'w') { |file| file.write(parsed_string) }
+        file_path
+      )
     end
 
 end
 
-KeywordParser.new.call
+KeywordParser.new.execute
