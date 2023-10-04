0d2832212699cc8b47aef6628c8776fcf438c0eb
Ashish Gaur <ashishgaurp91@gmail.com>
2023-10-04 10:58:29 +0530

    RUB-17 Use aliasing logic from ruby-vernac-parser class in language gem

--- lib/generators/language_alias_generator.rb
+++ lib/generators/language_alias_generator.rb
@@ -4,7 +4,7 @@ class LanguageAliasGenerator
     klass = Object.class_eval(class_name)
     singleton_klass = klass.singleton_class
 
-    singleton_klass.send(:alias_method, alias_name.to_sym, orig_name.to_sym)
+    singleton_klass.send(:alias_method, alias_name, orig_name)
   rescue StandardError => e
     puts e.message
   end
@@ -12,7 +12,7 @@ class LanguageAliasGenerator
   def alias_instance_method(class_name, orig_name, alias_name)
     klass = Object.class_eval(class_name)
 
-    klass.send(:alias_method, alias_name.to_sym, orig_name.to_sym)
+    klass.send(:alias_method, alias_name, orig_name)
   rescue StandardError => e
     puts e.message
   end
