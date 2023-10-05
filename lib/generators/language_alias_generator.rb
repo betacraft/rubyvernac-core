515f68c1a9ea726f4482f0d658c8f08deaa3acd5
Ashish Gaur <ashishgaurp91@gmail.com>
2023-10-05 12:01:59 +0530

    RUB-17 Remove catching errors in aliasing logic

--- lib/generators/language_alias_generator.rb
+++ lib/generators/language_alias_generator.rb
@@ -5,16 +5,12 @@ class LanguageAliasGenerator
     singleton_klass = klass.singleton_class
 
     singleton_klass.send(:alias_method, alias_name, orig_name)
-  rescue StandardError => e
-    puts e.message
   end
 
   def alias_instance_method(class_name, orig_name, alias_name)
     klass = Object.class_eval(class_name)
 
     klass.send(:alias_method, alias_name, orig_name)
-  rescue StandardError => e
-    puts e.message
   end
 
 end
