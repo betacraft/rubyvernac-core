921be65f011c57d14119b3d60ff3cab4ff763066
Ashish Gaur <ashishgaurp91@gmail.com>
2023-10-10 12:08:30 +0530

    RUB-23 Add language codes spec

---
+++ spec/templates/translator/language_codes_spec.rb
@@ -0,0 +1,20 @@
+RSpec.describe Rubyvernac::Translator::LanguageCodes do
+
+  subject do
+    Rubyvernac::Translator::LanguageCodes.new(
+      config_path: Dir.pwd + "/lib/templates/lib/available_languages.yml"
+    )
+  end
+
+  describe "allows available languages" do
+    it "allows hindi language" do
+      expect(subject.find_code('hindi')).to eq('hi')
+    end
+
+    it "allows spanish language" do
+      expect(subject.find_code('spanish')).to eq('es')
+    end
+
+  end
+
+end
