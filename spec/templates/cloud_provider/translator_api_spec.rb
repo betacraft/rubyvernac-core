1dd08963c0f3d023cf9dad34a3e7c3c5eb43f04f
Ashish Gaur <ashishgaurp91@gmail.com>
2023-10-11 12:16:42 +0530

    RUB-18 More fixes

--- spec/templates/cloud_provider/translator_api_spec.rb
+++ spec/templates/cloud_provider/translator_api_spec.rb
@@ -1,10 +1,9 @@
 RSpec.describe Rubyvernac::CloudProvider::TranslatorApi do
-
   describe "#translate" do
     it "delegate translate to cloud provider instance" do
       translator = instance_double("Rubyvernac::CloudProvider::Gcp::GoogleTranslatorApi")
-        expect(translator).to receive(:translate).with("text_to_be_translated", "hi").
-        and_return("text_after_translation")
+      expect(translator).to receive(:translate).with("text_to_be_translated", "hi")
+        .and_return("text_after_translation")
 
       translator_klass = class_double("Rubyvernac::CloudProvider::Gcp::GoogleTranslatorApi")
         .as_stubbed_const(transfer_nested_constants: true)
@@ -15,5 +14,4 @@ RSpec.describe Rubyvernac::CloudProvider::TranslatorApi do
       expect(instance.translate("text_to_be_translated", "hi")).to eq("text_after_translation")
     end
   end
-
 end
